e1a6123cf41f57b3aa73732e43984b48a63a1313
Flint <flintsmith@gmail.com>
2023-11-20 11:23:03 -0600

    1120

--- flask-blog.py
+++ flask-blog.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template, url_for
 from markupsafe import escape
+from forms import RegistrationForm. LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ec58e0a02e7d66b29d3c063d5a7293b8'
 
 posts =[
     {
@@ -27,6 +30,11 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template
 
 if __name__ == '__main__':
     app.run(debug =True)
