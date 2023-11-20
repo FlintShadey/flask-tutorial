eeaff3edd0782bca134f65c7fd2d2ad084337793
Flint <flintsmith@gmail.com>
2023-11-19 18:09:39 -0600

    11/19

--- flask-blog.py
+++ flask-blog.py
@@ -1,17 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
+posts =[
+    {
+        'author': 'Flint Smith',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Nov 19, 2023'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'second post content',
+        'date_posted': 'Nov 25, 2023'
+    }
+
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    name =request.args.get("name", "world")
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return'<h1>About</h1>'
+    return render_template('about.html', title = 'About')
 
 if __name__ == '__main__':
     app.run(debug =True)
